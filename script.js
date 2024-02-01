586fbeb8c94a0bd742b634a7f72421546aaa041b
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-02-01 11:41:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function makeid(l) {
   // write your code here
+	let result=" "
+let charList="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghigklmnopqrstuvwxyz0123456789"
+for(var i=0;i<l;i++){
+	result+=charList.charAt(
+		Math.foor(Math.random()*charList/length);
+	)
 }
+	return result
 
+
+}  
 // Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
