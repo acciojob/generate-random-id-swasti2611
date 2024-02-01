e3300d0624bc4bf907a60ccb8d987c9726e7e55e
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-02-01 11:56:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function makeid(l) {
   // write your code here
-	let res="";
-	let charList="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-	for (let index = 0; index <l ; index++) {
-		 res+=charList.charAt(
-			 Math.floor(
-			 Math.random()*charList.length;
-		 )
-		)
-		
-	}
-	return res;
+	 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+
+  for (let i = 0; i < l; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
 }
 
 // Do not change the code below.
